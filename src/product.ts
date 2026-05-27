214ec12eaa1869192e2dd38444ec0c4dfbda98c5
Kevin Sapang <kevin.sapang@gmail.com>
2026-05-27 13:34:24 +0800

    removed the product.ts

--- src/product.ts
+++
@@ -1,3 +0,0 @@
-// Redirecting to the centralized types file to match Atomic Design structure
-export type { Product, Category } from './types/product';
-export { CATEGORIES } from './types/product';
